63efb4dac563ff09bdba0461dfff940d04f61e95
Jey-Vaninah <hei.vaninah@gmail.com>
2025-02-02 20:07:25 +0100

    refactor: change migration

--- src/db/migration/0_create_role_library_admin.sql
+++ src/db/migration/0_create_role_library_admin.sql
@@ -1,20 +1,20 @@
 DO
-$create_role_library_role$
+$create_role_book$
 BEGIN
         IF EXISTS (
-            SELECT FROM pg_catalog.pg_roles WHERE  rolname = 'library_role') THEN
-            RAISE NOTICE 'Role "library_role" already exists. Skipping.';
+            SELECT FROM pg_catalog.pg_roles WHERE  rolname = 'book') THEN
+            RAISE NOTICE 'Role "book" already exists. Skipping.';
 ELSE
-CREATE ROLE library_role LOGIN PASSWORD '123456';
+CREATE ROLE book LOGIN PASSWORD '123456';
 END IF;
 END
-$create_role_library_role$;
+$create_role_book$;
 
-GRANT ALL PRIVILEGES ON DATABASE library TO library_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE library_role
-    GRANT ALL PRIVILEGES ON TABLES TO library_role;
-ALTER DEFAULT PRIVILEGES FOR ROLE library_role
-    GRANT ALL PRIVILEGES ON SEQUENCES TO library_role;
-GRANT ALL PRIVILEGES ON SCHEMA public TO library_role;
-\c library library_role
+GRANT ALL PRIVILEGES ON DATABASE management TO book;
+ALTER DEFAULT PRIVILEGES FOR ROLE book
+    GRANT ALL PRIVILEGES ON TABLES TO book;
+ALTER DEFAULT PRIVILEGES FOR ROLE book
+    GRANT ALL PRIVILEGES ON SEQUENCES TO book;
+GRANT ALL PRIVILEGES ON SCHEMA public TO book;
+\c management book
 
